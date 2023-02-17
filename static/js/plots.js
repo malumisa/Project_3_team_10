b99865f905214209a55a5f0cb2dca8806412bcff
Sunday Akiyesi <akiyesis@gmail.com>
2023-02-16 21:32:16 -0500

    update repo

--- static/js/plots.js
+++ static/js/plots.js
@@ -2,7 +2,7 @@ console.log(cities);
 
 const options = {
     method: 'GET',
-    url:'http://api.weatherapi.com/v1/current.json?key=29ad3cbdd70c4198ac704843231502 &q=bulk.json'
+    url:'http://api.weatherapi.com/v1/current.json?key=29ad3cbdd70c4198ac704843231502 &q=Montreal&.json'
     // url: 'https://adsbexchange-com1.p.rapidapi.com/v2/registration/N8737L/',
     // headers: {
     //   // 'X-RapidAPI-Key': api_key,
@@ -10,13 +10,41 @@ const options = {
     // }
   };
   
-  const dataPromise = d3.json(options);
+  const dataPromise = d3.json(options.url);
   console.log("Data Promise: ", dataPromise);
 
 // Fetch the JSON data and console log it
-  d3.json(options).then(function(data) {
+  d3.json(options.url).then(function(data) {
   console.log(data);
 });
+
+for(var i=0;i<cities.length;i++){
+  // console.log(cities.length);
+  row=cities[i];
+  let bulk=row.city
+  const options = {
+    method: 'GET',
+    url:`http://api.weatherapi.com/v1/current.json?key=29ad3cbdd70c4198ac704843231502 &q=${bulk}&.json`
+    // url: 'https://adsbexchange-com1.p.rapidapi.com/v2/registration/N8737L/',
+    // headers: {
+    //   // 'X-RapidAPI-Key': api_key,
+    //   // 'X-RapidAPI-Host': 'adsbexchange-com1.p.rapidapi.com'
+    // }
+  };
+  
+  const dataPromise = d3.json(options.url);
+  console.log("Data Promise: ", dataPromise);
+
+// Fetch the JSON data and console log it
+  d3.json(options.url).then(function(data) {
+    // console.log(data);
+    if (data.location['country'] === 'Canada') {
+      console.log(data.location);
+    }
+});
+};
+
+
 
 
 
