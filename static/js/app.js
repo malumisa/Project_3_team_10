4e53208790babc0fa0560b6590bca1113afe9ce2
Sunday Akiyesi <akiyesis@gmail.com>
2023-02-25 11:36:03 -0500

    update repo

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,9 @@
 // json url
-console.log(cities);
-
+// console.log(cities);
+var city=[];
+var temp =[];
+var pres=[];
+var hum =[];
 for(var i=0;i<cities.length;i++){
         row=cities[i];
         let bulk=row.city
@@ -14,196 +17,125 @@ for(var i=0;i<cities.length;i++){
 // Fetch the JSON data and console log it
         d3.json(options.url).then(function(data) {
     // console.log(data);
-        let city=[];
+        
         if (data.location['country'] === 'Canada') {
+            // console.log(data)
         city.push(data.location['name']);
+        temp.push(data.current['temp_c']);
+        pres.push(data.current['pressure_in']);
+        hum.push(data.current['humidity']);
+        
+        }
         // console.log(city);
-    }
+        // console.log(temp);
+        // console.log(pres);
+        // console.log(hum);
+        // initialize function to pull city names
+function init() {
+    let selector = d3.select("#selDataset");
+    // retrieve JSON data
+    let sampleNames=city;
+    // console.log(sampleNames);
+    sampleNames.forEach((sample) =>{
+                selector
+                .append("option")
+                .text(sample)
+                .property("value",sample)
+            });
+            var firstSample = sampleNames[0];
+            buildCharts(firstSample);
+            buildMetadata(firstSample);
+
+
+function buildCharts(sample) {
+    // variables for charts
+    var sampleValuesSlice = temp.slice(0,10).reverse();
+    var samplecityname=city.slice(0,10).reverse();
+    // console.log(sampleValuesSlice);
+    // console.log(samplecityname);
+    //----------------------------------------------//
+   //----------------------------------------------//
+// // Trace1 for the city Data
+let trace1 = {
+  x: sampleValuesSlice,
+  y: samplecityname,
+  text: sampleValuesSlice,
+  name: "Canadian Cities",
+  type: "bar",
+  orientation: "h"
+};
+
+// Data array
+
+let traceData = [trace1];
+
+// Apply a title to the layout
+let layout = {
+  title: "Top Ten-City Temperature",
+  
+};
+
+Plotly.newPlot("plot", traceData, layout);
+
+    //---------------------------------------------------------//
+    //----------------------------------------------------------//
+                // Building  a bubble chart//
+        // var firsttrace = {
+        //     x: otuIds,
+        //     y: sampleValues,
+        //     mode: "markers",
+        //     marker: {
+        //         size: sampleValues,
+        //         color: otuIds,
+        //         colorscale: "Earth"
+        //     },
+        //     text: otuIds
+        // };
+        // var bubdata = [firsttrace];
+        // var bublayout = {title: "Bacteria Cultures Per Sample",
+        // xaxis: {title:"OTU ID"},
+        // // hovermode = otu_labels
+        //     showlegend: false
+        // };
+
+        // Plotly.newPlot("bubble", bubdata, bublayout);
+    };
+};
+    // call initialize function to run
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+init();
 });
 };
 
-// initialize function to pull city names
-function init() {
-        var dropDown = d3.select("#selDataset");
-        // retrieve JSON data
-        d3.json(options.url).then(function (data) {
-        if(data.location['country']==='Canada'){
-                var sampleNames=data.location['name'];
-                console.log(sampleNames);
-                sampleNames.forEach((sample) =>{
-                dropDown.append("option").text(sample).property("value",sample)
-                });
-                var initSample=sampleNames[0];
-                buildDemo(initSample);
-                buildCharts(initSample);
-        }
-        //     var sampleNames = city;
-        //     console.log(sampleNames)
-        //     sampleNames.forEach((sample) => {
-        //         dropDown.append("option").text(sample).property("value", sample)
-        //     });
-        //     var initSample = sampleNames[0];
-        //     buildDemo(initSample);
-        //     buildCharts(initSample);
-        });
-    };
-    
-    //----------------------------------------------------//
-    //---------------------------------------------------//
-            //Building a function to create charts// 
-    //Deliverable 1
-    //---------------------------------------------------//
-//     function buildCharts(sample) {
-//         d3.json(url).then(function (data) {
-//             // variables for charts
-//             var samplesComplete = data.samples;
-//             var sampleInfo = samplesComplete.filter(row => row.id == sample);
-//             var sampleValues = sampleInfo[0].sample_values;
-//             var sampleValuesSlice = sampleValues.slice(0,10).reverse();
-//             var otuIds = sampleInfo[0].otu_ids;
-//             var otuIdsSlice = otuIds.slice(0,10).reverse();
-//             var otuLabels = sampleInfo[0].otu_labels;
-//             var otuLabelsSlice = otuLabels.slice(0,10).reverse();
-//             var metaData = data.metadata;
-//             var metaDataSample = metaData.filter(row => row.id == sample);
-//             var wash = metaDataSample[0].wfreq;
-    
-        //----------------------------------------------//
-        //----------------------------------------------//
-                    // Building a bar chart//
-        //----------------------------------------------//
-        //     var trace1 = {
-        //         x: sampleValuesSlice,
-        //         y: otuIdsSlice.map(item => `OTU ${item}`),
-        //         type: "bar",
-        //         orientation: "h",
-        //         text: otuLabelsSlice,
-        //     };
-        //     var data = [trace1];
-        //     Plotly.newPlot("bar", data)
-    
-        //---------------------------------------------------------//
-        //----------------------------------------------------------//
-                    // Building  a bubble chart//
-        //     var firsttrace = {
-        //         x: otuIds,
-        //         y: sampleValues,
-        //         mode: "markers",
-        //         marker: {
-        //             size: sampleValues,
-        //             color: otuIds,
-        //             colorscale: "Earth"
-        //         },
-        //         text: otuIds
-        //     };
-        //     var bubdata = [firsttrace];
-        //     var bublayout = {title: "Bacteria Cultures Per Sample",
-        //     xaxis: {title:"OTU ID"},
-        //     // hovermode = otu_labels
-        //         showlegend: false
-        //     };
-    
-        //     Plotly.newPlot("bubble", bubdata, bublayout);
-    
-            //---------------------------------------------------------------//
-            //---------------------------------------------------------------//
-                             // BUilding a gauge chart//
-            //---------------------------------------------------------------//
-            // determine angle for each wfreq segment on the chart
-            //   var angle = (4/ 9) * 180;
-            //   // calculate end points for triangle pointer path
-            // var degrees = 180 - angle,
-            // radius = .5;
-            // var radians = degrees * Math.PI / 180;
-            // var x = radius * Math.cos(radians);
-            // var y = radius * Math.sin(radians);
-    
-            // // // Path: to create needle shape (triangle). Initial coordinates of two of the triangle corners plus the third calculated end tip that points to the appropriate segment on the gauge 
-            // // // M aX aY L bX bY L cX cY Z
-            // // var mainPath = 'M -.0 -0.005 L .0 0.025 L ',
-            // //     cX = String(x),
-            // //     cY = String(y),
-            // //     pathEnd = ' Z';
-            // // var path = mainPath + cX + " " + cY + pathEnd;
-            // // gaugeColors = ['rgb(8,29,88)', 'rgb(37,52,148)', 'rgb(34,94,168)', 'rgb(29,145,192)', 'rgb(65,182,196)', 'rgb(127,205,187)', 'rgb(199,233,180)', 'rgb(237,248,217)', 'rgb(255,255,217)', 'white']
-            // // // create a trace to draw the circle where the needle is centered
-            // // var traceNeedleCenter = {
-            // //     type: 'scatter',
-            // //     showlegend: false,
-            // //     x: [0],
-            // //     y: [0],
-            // //     marker: { size: 35, color: '850000' },
-            // //     name: wash,
-            // //     hoverinfo: 'name'
-            // };
-        //     var datagauge = [
-        //         {
-        //           domain: { x: [0, 1], y: [0, 1] },
-        //           value: wash,
-        //           title: {text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week" },
-        //           type: "indicator",
-        //           mode: "gauge+number",
-        //           gauge: {
-        //             axis: { range: [null, 9] },
-        //             bar: { color: "red" },
-        //             steps: [
-                    //   { range: [0, 1], color: "rgba(114, 70, 28, 0.9)" },
-                    //   { range: [1, 2], color: "rgba(169, 146, 48, 0.9)" },
-                    //   { range: [2, 3], color: "rgba(199, 188, 58, 0.9)" },
-                    //   { range: [3, 4], color: "rgba(221, 226, 68, 0.9)" },
-                    //   { range: [4, 5], color: "rgba(66, 206, 58, 0.9)" },
-                    //   { range: [5, 6], color: "rgba(29, 184, 122, 0.9)" },
-                    //   { range: [6, 7], color: "rgba(15, 155, 165, 0.9)" },
-                    //   { range: [7, 8], color: "rgba(11, 117, 154, 0.9)" },
-                    //   { range: [8, 9], color: "rgba(0, 34, 125, 0.9)" },
-//                     //   { range: [0, 1], color: "red" },
-//                     { range: [1, 2], color: "orange" },
-//                     { range: [2, 3], color: "yellow" },
-//                     { range: [3, 4], color: "beige" },
-//                     { range: [4, 5], color: "lightyellow" },
-//                     { range: [5, 6], color: "powderblue" },
-//                     { range: [6, 7], color: "lightskyblue" },
-//                     { range: [7, 8], color: "lightgreen" },
-//                     { range: [8, 9], color: "green" }
-//                     ],
-//                     threshold: {
-//                       line: { color: "black", width: 4 },
-//                       thickness: 0.75,
-//                       value: wash
-//                     }
-//                   }
-//                 }
-//               ];
-              
-//               var layout2 = { width: 800, height: 450, margin: { t: 0, b: 0 } };
-//               Plotly.newPlot('gauge', datagauge, layout2);
-//         });
-//     };
-    
-    //--------------------------------------------------------------//
-    //--------------------------------------------------------------//
-          // Building a Weather Elements//
-    //-------------------------------------------------------------//
-//     function buildDemo(sample) {
-//         var demo = d3.select("#sample-metadata");
-//         d3.json(url).then(function (data) {
-//             var metaData = data.metadata;
-//             var metaDataSample = metaData.filter(row => row.id == sample);
-//             demo.selectAll("p").remove();
-//             metaDataSample.forEach((row) => {
-//                 for (const [key, value] of Object.entries(row)) {
-//                     demo.append("p").text(`${key}: ${value}`);
-//                 };
-//             });
-//         });
-//     };
-    
-    // 
-//     function optionChanged(sample) {
-//         buildDemo(sample);
-//         buildCharts(sample);
-//     };
-    
-    // call initialize function to run
-    init();
-   
+
+
+
--- static/js/plots.js
+++ static/js/plots.js
@@ -19,18 +19,13 @@ for(var i=0;i<cities.length;i++){
 });
 };
 
-// //31acb2c094e3b8adc5fe6b3a88b35505 
-
-// const apiKey = "31acb2c094e3b8adc5fe6b3a88b35505";  
-// const KELVIN = 273; 
-
 let d = new Date();
 let date = d.getDate();
 // console.log(date)
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let month = months[d.getMonth()];
 let year = d.getFullYear();
-document.getElementById('date').innerHTML = date + " "+ month + " " + year;
+document.getElementById("dat").innerHTML = date + " " + month +" " +year;
 
 
 if('geolocation' in navigator){  
@@ -43,10 +38,10 @@ function setPosition(position){
     let latitude = position.coords.latitude;  
     let longitude = position.coords.longitude;  
       
-    checkWeather(latitude, longitude) 
+    Weatherstatus(latitude, longitude) 
 }  
 
-function checkWeather(latitude, longitude) {
+function Weatherstatus(latitude, longitude) {
 
 let api = `http://api.weatherapi.com/v1/current.json?key=29ad3cbdd70c4198ac704843231502 &q=${bulk}&`
 // console.log(api);
@@ -54,19 +49,19 @@ fetch(api)
 .then(response => response.json())
 .then(data => {
     if (data.location['country'] === 'Canada') {
-        let temp = Math.floor(data.condition['temp_c']);  
+        let temp = Math.floor(data.current['temp_c']);  
         let city = data.location['name'];
-        let feels_like = Math.floor(data.condition['feelslike_c']);
-        let status = data.condition[0].text;
-        let icon = data.condition[0].icon;
-        let windSpeed =data.condition['wind_mph']; 
+        let feels_like = Math.floor(data.current['feelslike_c']);
+        let status = data.current[0].text;
+        let icon = data.current[0].icon;
+        let windSpeed =data.current['wind_mph']; 
 
     document.getElementById('city').innerHTML = city;
     document.getElementById('temp').innerHTML = temp;
     document.getElementById('status').innerHTML = status;
     document.getElementById('feelsLike').innerHTML = feels_like + "°C";
     document.getElementById('windSpeed').innerHTML = windSpeed +"m/h";
-    document.getElementById('icon').src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';  
+    document.getElementById('icon').src = `https:${icon}`;  
 
 }})
 .catch(err => console.log(err))
@@ -75,30 +70,33 @@ fetch(api)
 
 function locate() {
 
-    let location = document.getElementById('location').value;
+    let row_location = document.getElementById('location').value;
 
-    let loacateThis = `http://api.weatherapi.com/v1/current.json?key=29ad3cbdd70c4198ac704843231502 &q=${bulk}&`
+    let loacateThis = `http://api.weatherapi.com/v1/current.json?key=29ad3cbdd70c4198ac704843231502 &q=${row_location}&`
      
     fetch(loacateThis)
     .then(response => response.json())
     .then(data => {
         if (data.location['country'] === 'Canada') {
     
-            let temp = Math.floor(data.condition['temp_c']);  
+            let temp = Math.floor(data.current['temp_c']);  
             let city = data.location['name'];
-            let feels_like = Math.floor(data.condition['feelslike_c']);
-            let status = data.condition[0].text;
-            let icon = data.condition[0].icon;
-            let windSpeed =data.condition['wind_mph']; 
+            let feels_like = Math.floor(data.current['feelslike_c']);
+            let status = data.current.condition.text;
+            let icon = data.current.condition.icon;
+            let windSpeed =data.current['wind_mph']; 
     
         document.getElementById('city').innerHTML = city;
         document.getElementById('temp').innerHTML = temp;
         document.getElementById('status').innerHTML = status;
         document.getElementById('feelsLike').innerHTML = feels_like + "°C";
         document.getElementById('windSpeed').innerHTML = windSpeed +"m/h";
-        document.getElementById('icon').src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'; 
+        document.getElementById('icon').src = `https:${icon}`; 
        
     
     }})
-    .catch(err => console.log(err))
+    // .catch(err => console.log(err))
     }
+
+
+   
